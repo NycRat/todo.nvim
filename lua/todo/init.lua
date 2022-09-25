3e42c095828e22ef5f874bd65a2231b1dc8fcda5
Eric Xiao <ericxiao0000@gmail.com>
2022-09-25 08:11:00 -0700

    Allows Multiple Todo Files

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -4,6 +4,8 @@ local data_path = vim.fn.stdpath("data")
 local todo_path = data_path.."/todo.nvim/"
 local default_todo_file = todo_path.."todo.txt"
 
+local default_file_extension = ".txt"
+
 local file = io.open(default_todo_file, "a")
 if (file == nil) then
   os.execute("mkdir "..todo_path)
@@ -12,11 +14,30 @@ else
   io.close(file)
 end
 
+local function get_file_extension(file_path)
+  local arr = {}
+  for word in file_path:gmatch('[^.%s]+') do
+    table.insert(arr, word)
+  end
+  if (#arr == 1) then
+    return nil
+  else
+    return arr[#arr]
+  end
+end
+
 M.open = function (todo_file)
-  if (todo_file == nil) then
+  if (todo_file == nil or todo_file == "") then
     vim.cmd("e "..default_todo_file)
   else
-    vim.cmd("e "..todo_path..default_todo_file)
+    local file_extension = get_file_extension(todo_file)
+
+    if file_extension == nil then
+      todo_file = todo_file..default_file_extension
+    end
+
+    vim.cmd("e "..todo_path..todo_file)
+    print("TODO FILE: "..todo_file)
   end
 end
 
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,9 @@
 require("todo")
 
-vim.cmd[[
-  command! TodoOpen lua require("todo").open()
-]]
+vim.api.nvim_create_user_command("TodoOpen", function (cmd)
+  require("todo").open(cmd.args)
+end, { nargs = "?" })
+
+-- vim.cmd[[
+--   command! TodoOpen lua require("todo").open()
+-- ]]
