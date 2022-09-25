f4e8c4b315708dd091830bac19e88f58d59fa9e3
Eric Xiao <ericxiao0000@gmail.com>
2022-09-25 08:50:13 -0700

    Refactor

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -2,12 +2,12 @@ local M = {}
 
 local data_path = vim.fn.stdpath("data")
 local todo_path = data_path.."/todo.nvim/"
-local default_todo_file = todo_path.."todo.txt"
+local default_todo_file = "todo.txt"
 
-local file = io.open(default_todo_file, "a")
+local file = io.open(todo_path..default_todo_file, "a")
 if (file == nil) then
   os.execute("mkdir "..todo_path)
-  io.open(default_todo_file, "w")
+  io.open(todo_path..default_todo_file, "w")
 else
   io.close(file)
 end
@@ -38,7 +38,7 @@ end
 
 M.open = function(todo_file)
   if (todo_file == nil or todo_file == "") then
-    vim.cmd("e "..default_todo_file)
+    todo_file = default_todo_file
   else
     local file_extension = get_file_extension(todo_file)
 
@@ -46,9 +46,10 @@ M.open = function(todo_file)
       todo_file = todo_file..M.options.default_file_extension
     end
 
-    vim.cmd("e "..todo_path..todo_file)
-    print("TODO FILE: "..todo_file)
+    todo_file = todo_file
   end
+  vim.cmd("e "..todo_path..todo_file)
+  print("OPENED TODO FILE:"..todo_file)
 end
 
 return M
