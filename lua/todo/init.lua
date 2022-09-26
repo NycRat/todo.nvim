6daa1178aca5691d8b83ac75048c094aceae0942
Eric Xiao <ericxiao0000@gmail.com>
2022-09-25 22:04:38 -0700

    Todo file list, opening file by index

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -1,13 +1,22 @@
 local M = {}
 
-local data_path = vim.fn.stdpath("data")
-local todo_path = data_path.."/todo.nvim/"
-local default_todo_file = "todo.txt"
+local data_path = vim.fn.stdpath("data").."/"
+local todo_path = data_path.."todo.nvim/"
+local default_todo_file_path = "todo.txt"
+local todo_file_list_file_path = "todo_list_list.txt";
 
-local file = io.open(todo_path..default_todo_file, "a")
+local file = io.open(todo_path..default_todo_file_path, "a")
 if (file == nil) then
   os.execute("mkdir "..todo_path)
-  io.open(todo_path..default_todo_file, "w")
+  io.open(todo_path..default_todo_file_path, "w")
+else
+  io.close(file)
+end
+
+file = io.open(todo_path..todo_file_list_file_path, "a")
+if (file == nil) then
+  os.execute("mkdir "..todo_path)
+  io.open(todo_path..todo_file_list_file_path, "w")
 else
   io.close(file)
 end
@@ -65,9 +74,13 @@ M.push_previous_file = function(current_path)
   M.previous_file = current_path
 end
 
+M.openTodoFileList = function()
+  vim.cmd("e "..data_path..todo_file_list_file_path)
+end
+
 M.open = function(todo_file)
   if (todo_file == nil or todo_file == "") then
-    todo_file = default_todo_file
+    todo_file = default_todo_file_path
   else
     local file_extension = get_file_extension(todo_file)
 
@@ -79,7 +92,33 @@ M.open = function(todo_file)
   end
 
   vim.cmd("e "..todo_path..todo_file)
-  print("OPENED TODO FILE: "..todo_file)
+  print("Opened Todo File: "..todo_file)
+end
+
+M.openIndex = function(file_index)
+  file_index = tonumber(file_index)
+  if (file_index > 10000 or file_index < 1) then
+    -- no way you have this many todo files
+    return
+  end
+
+  local todo_file_list_file = io.open(data_path..todo_file_list_file_path, "r")
+  io.input(todo_file_list_file)
+
+  local i = 1
+  local line = ""
+  while true do
+    line = io.read("l")
+    if (line == nil) then
+      break
+    end
+    if (i == file_index) then
+      vim.cmd("e "..todo_path..line)
+      print("Opened Todo File: "..line)
+      break
+    end
+    i = i + 1
+  end
 end
 
 M.close = function(current_path)
@@ -88,6 +127,7 @@ M.close = function(current_path)
       M.previous_file = ".";
     end
     vim.cmd("e "..M.previous_file)
+    print("Closed Todo File")
   end
 end
 
--- plugin/init.lua
+++ plugin/init.lua
@@ -2,10 +2,23 @@ require("todo")
 require("todo").setup({
   default_file_extension = ".txt"
 })
+
+vim.api.nvim_create_user_command("TodoOpenFileList", function (cmd)
+  local path = vim.fn.expand("%:F")
+  require("todo").push_previous_file(path)
+  require("todo").openTodoFileList()
+end, {})
+
 vim.api.nvim_create_user_command("TodoOpen", function (cmd)
   local path = vim.fn.expand("%:F")
   require("todo").push_previous_file(path)
   require("todo").open(cmd.args)
+end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("TodoOpenIndex", function (cmd)
+  local path = vim.fn.expand("%:F")
+  require("todo").push_previous_file(path)
+  require("todo").openIndex(cmd.args)
 end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("TodoClose", function (cmd)
